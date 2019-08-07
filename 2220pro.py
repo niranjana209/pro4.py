4a56073ab915b8c68ac5735afa8ac03700d652de
niranjana209 <52314670+niranjana209@users.noreply.github.com>
2019-08-07 02:39:47 -0700

    Update 2220pro.py

--- 2220pro.py
+++ 2220pro.py
@@ -5,13 +5,13 @@ mosd.reverse()
 ssss=kng
 pend=0
 for i in mosd:
-    if(s>=i):
+    if(ssss>=i):
         any=int(ssss/i)
         pend=pend+any
         ssss=ssss-any*i
     if ssss==0:
         break
-if(s==0):
+if(ssss==0):
    print(pend)
 else:
    print("it's not posiible to select coins in such a way that they sum upto",kng)
