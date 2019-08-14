0d999a740fa94869fbe93a91a4cdf7687c733955
niranjana209 <52314670+niranjana209@users.noreply.github.com>
2019-08-14 10:43:10 +0530

    Create 209pro.py

---
+++ 209pro.py
@@ -0,0 +1,15 @@
+ccc=int(input())
+zen=0
+xon=0
+bhoo=[]
+while zen<90 and zen<ccc:
+  son=0
+  for j in str(ccc-zen):
+    son+=int(j)
+  if s+(ccc-zen)==ccc:
+    xon+=1
+    bhoo.append(ccc-zen)
+  zen+=1
+print(xon)
+for zen in bhoo:
+  print(zen)
